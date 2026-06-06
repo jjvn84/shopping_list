8468dde0ff1fbefdacc0c71bfc1ae1171a28af2e
Juan Vásquez <jjvn84@gmail.com>
2026-06-05 20:21:43 -0400

    Added flake.lock and updated flake.nix to include the new dependencies. Also updated .gitignore to ignore the lock file.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Entorno Jupyter con uv y automatización de librerías mediante nix-ld";
+  description = "Entorno de desarrollo básico para Rust con rust-analyzer y clippy";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -9,7 +9,9 @@
     { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
@@ -19,11 +21,23 @@
           rustfmt
           clippy
           rust-analyzer
+          pkg-config
+
+          # System Libraries
+          cairo
+          glib
+          gtk3
+          libsoup_3
+          openssl
+          webkitgtk_4_1
+          xdotool
         ];
-
-        # Tell rust-analyzer where to find the standard library source
+        # OpenSSL Environment Variables
+        OPENSSL_DIR = "${pkgs.openssl.dev}";
+        OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+        OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.glib.dev}/lib/pkgconfig:${pkgs.cairo.dev}/lib/pkgconfig:${pkgs.gtk3.dev}/lib/pkgconfig:${pkgs.libsoup_3.dev}/lib/libsoup-3.0.pc:${pkgs.webkitgtk_4_1.dev}/lib/pkgconfig:${pkgs.xdotool}/lib/pkgconfig";
         RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-
       };
     };
 }
